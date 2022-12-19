95f9b3d5f516d1e0a442e75342827f327e7986b1
Shahin-Alz <shaalemzadeh@gmail.com>
2022-12-19 11:21:22 +0100

    finish

---
+++ public/js/main.js
@@ -0,0 +1,58 @@
+// exo1
+let a;
+let b;
+let c;
+
+let d;
+let e;
+let f;
+// affecter
+
+a = `j'aime les tartines et jen mange`;
+b = `4 par jour`;
+c = `${a} ${b}`;
+
+d = 5;
+e = 35;
+f = d + e;
+
+// afficher
+
+console.log(a);
+console.log(c);
+console.log(f);
+
+console.log(a + d);
+
+
+// exo2
+let aa;
+let bb;
+
+// affecter
+
+aa = `Salut tout le monde`;
+bb = `Au revoir`
+aa = bb
+
+// afficher
+
+console.log(aa);
+
+
+// exo3
+let aaa;
+let bbb;
+
+// affecter
+aaa = 4;
+bbb ="6";
+
+// afficher
+
+console.log(aaa>bbb);
+console.log(aaa<bbb);
+console.log(aaa==bbb);
+console.log(aaa!=bbb);
+console.log(aaa===bbb);
+console.log(aaa!==bbb);
